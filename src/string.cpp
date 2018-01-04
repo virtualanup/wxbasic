99a60e604dc09d6fcbf242ad2936b8cddd8851e4
Anup Pokhrel <virtualanup@gmail.com>
2018-01-04 01:29:42 -0500

    Load string in constructor

--- src/string.cpp
+++ src/string.cpp
@@ -2,7 +2,7 @@
 
 namespace wxbasic {
 WString::WString(const std::string &val)
-    : Variant(false, VariantType::STRING) {}
+    : Variant(false, VariantType::STRING), value(val) {}
 WString::~WString() {}
 
 } // namespace wxbasic
